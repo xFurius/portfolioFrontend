fc573ca825e68a13168d4e9aa8c2321d722f6200
xFurius <matip03211@gmail.com>
2025-10-11 19:42:17 +0200

    added button style based on active section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,6 +5,10 @@ import GitHub from "../assets/github.svg?react"
 import Mail from "../assets/mail.svg?react"
 
 export default function Nav(){
+    const scrollIntoView = (id) => {
+        document.getElementById(id).scrollIntoView({behavior: 'smooth'})
+    }
+
     return(
         <>
         <div className="wrapper">
@@ -16,15 +20,15 @@ export default function Nav(){
                 <span>EN</span>
             </div>
             <nav>
-                <button>about me</button>
-                <button>skills</button>
-                <button>projects</button>
-                <button>qualifications</button>
+                <button id="btn-aboutme" onClick={() => scrollIntoView('section1')}>about me</button>
+                <button id="btn-skills" onClick={() => scrollIntoView('section2')}>skills</button>
+                <button id="btn-projects" onClick={() => scrollIntoView('section3')}>projects</button>
+                <button id="btn-qual" onClick={() => scrollIntoView('section4')}>qualifications</button>
             </nav>
             <div className='links'>
-                <LinkedIn/>
-                <GitHub/>
-                <Mail/>
+                <a href="https://www.linkedin.com/in/mateusz-pszczoła-328a6327b" target="_blank"><LinkedIn/></a>
+                <a href="https://github.com/xFurius" target="_blank"><GitHub/></a>
+                <a href="mailto:pszczola.mateusz1@gmail.com" target="_blank"><Mail/></a>
             </div>
             </div>
         </div>
