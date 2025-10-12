c05649db44bf1790610eedc31185124e9d59f109
xFurius <matip03211@gmail.com>
2025-10-12 16:48:27 +0200

    added home page for en version

---
+++ src/components/en/Home.jsx
@@ -0,0 +1,94 @@
+import "../../css/home.css"
+import Nav from "./Nav";
+import AboutMe from "../AboutMe";
+import PortfolioSVG from "../../assets/portfolio.svg?react"
+import Divider from "../../assets/sectionDvdr.svg?react"
+import SectionIndicator from "../SectionIndicator";
+import { useEffect, useRef } from "react";
+
+// http://localhost:5173/pl/projects
+// http://localhost:5173/en/projects
+
+export default function Home(){
+    const section1 = useRef(null);
+    const section2 = useRef(null);
+    const section3 = useRef(null);
+    const section4 = useRef(null);
+
+    const sections = [
+        { ref: section1, id: 'aboutme' },
+        { ref: section2, id: 'skills' },
+        { ref: section3, id: 'projects' },
+        { ref: section4, id: 'qual' },
+    ];
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+            const id = entry.target.dataset.id;
+            const dot = document.getElementById(`dot-${id}`)
+            const btn = document.getElementById(`btn-${id}`)
+
+            if (dot && btn) {
+                if (entry.isIntersecting) {
+                    dot.classList.add('active');
+                    btn.classList.add('active')
+                } else {
+                    dot.classList.remove('active');
+                    btn.classList.remove('active')
+                }
+            }
+            });
+        },
+        {
+            threshold: 0.6,
+        }
+        );
+
+        sections.forEach(({ ref, id }) => {
+        if (ref.current) {
+            ref.current.dataset.id = id;
+            observer.observe(ref.current);
+        }
+        });
+
+        return () => {
+        sections.forEach( ref  => {
+            if (ref.current) {
+            observer.unobserve(ref.current);
+            }
+        });
+        };
+    }, []);
+        
+    return(
+        <>
+            <SectionIndicator/>
+            <section>
+                <Nav/>
+            </section>
+            <main>
+                <section>
+                    <PortfolioSVG/>
+                    <Divider/>
+                </section>
+                <section ref={section1} id="section1">
+                    <AboutMe/>
+                    <Divider/>
+                </section>
+                <section ref={section2} id="section2">
+                    <PortfolioSVG/>
+                    <Divider/>
+                </section>
+                <section ref={section3} id="section3">
+                    <PortfolioSVG/>
+                    <Divider/>
+                </section>
+                <section ref={section4} id="section4">
+                    <PortfolioSVG/>
+                    <Divider/>
+                </section>
+            </main>
+        </>
+    )
+}
