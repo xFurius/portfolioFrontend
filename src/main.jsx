3443cf78ccb072f3fc542c31299f1b651c8ec468
xFurius <matip03211@gmail.com>
2025-10-14 15:46:10 +0200

    added default route

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from "./components/en/Home.jsx"
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
+    path: "*",
+    element: <Navigate to="/en/home" replace/>
+  },{
     path: "/en/home",
     element: <Home/>,
     // children:[
