5047026f4d73d0a6b92be0d9c52f3b32ebc53572
xFurius <matip03211@gmail.com>
2025-10-26 21:34:25 +0100

    work section finish

--- src/components/QualComponent.jsx
+++ src/components/QualComponent.jsx
@@ -4,7 +4,6 @@ export default function QualComponent({text, desc}){
             <div>
                 <span>{text}</span>
                 <span>{desc}</span>
-                <button>See more</button>
             </div>
         </>
     )
--- src/components/en/Qualifications.jsx
+++ src/components/en/Qualifications.jsx
@@ -10,16 +10,31 @@ export default function Qualifications(){
                 <span>qualifications</span>
                 <p>Here you can see my work experience, education history as well as my certificates</p>
             </div>
-            <div className={styles.work_wrapper}>
+            <div className={styles.wrapper}>
                 <div>
-                    <span>work experience</span>
+                    <span style={{"--m-left": "-340px", "--w":"350px"}}>work experience</span>
                 </div>
                 <div className={styles.experience_wrapper}>
                     <div className={styles.qual_comp_wrapper}>
                         <QualComponent text={"JUL 2024 - present"} desc={"Infrastructure Monitoring Analyst, Capgemini"}/>
+                        <div style={{"--w-left": "0%", "--t-align": "left"}}>
+                            <ul>
+                                <li>Monitoring client's infrastructure by reacting to alerts received on PagerDuty, SolarWinds Orion, mailbox and MonteCarlo</li>
+                                <li>Monitoring workflows on Informatica and GCP Airflow</li>
+                                <li>Managing backup jobs on NetBackup</li>
+                                <li>Developing and maintaining chrome extension which automates some of the team's tasks and gathers data for reports</li>
+                            </ul>
+                        </div>
                     </div>
                     <div className={styles.qual_comp_wrapper}>
-                        <QualComponent text={"JUL 2024 - present"} desc={"Infrastructure Monitoring Analyst, Capgemini"}/>
+                        <QualComponent text={"FEB 2024 - JUL 2024"} desc={"Service Desk Analys, Capgemini"}/>
+                        <div style={{"--w-left": "10%", "--d": "rtl"}}>
+                            <ul>
+                                <li>Assisting users with technical problems via phone, email and remote connection</li>
+                                <li>Tracking issues in ServiceNow</li>
+                                <li>Taking care of incidents in a timely manner according to SLA</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
