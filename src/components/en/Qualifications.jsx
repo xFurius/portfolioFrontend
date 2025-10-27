0117c43e52288750ecf1534e0f70f4c602d0a237
xFurius <matip03211@gmail.com>
2025-10-27 17:56:44 +0100

    qualifications section finish

--- src/components/en/Qualifications.jsx
+++ src/components/en/Qualifications.jsx
@@ -1,5 +1,6 @@
 import QualComponent from "../QualComponent"
 import QualIcon from "../../assets/qualIcon.svg?react"
+import CertIcon from "../../assets/cert_icon.svg?react"
 import styles from "../../css/qualifications.module.css"
 
 export default function Qualifications(){
@@ -35,6 +36,47 @@ export default function Qualifications(){
                                 <li>Taking care of incidents in a timely manner according to SLA</li>
                             </ul>
                         </div>
+                    </div>
+                </div>
+                <div>
+                    <span style={{"--m-left": "-230px", "--w":"250px"}}>education</span>
+                </div>
+                <div className={styles.experience_wrapper}>
+                    <div className={styles.qual_comp_wrapper}>
+                        <QualComponent text={"SEP 2023 - present"} desc={"Computer Science, Lublin University of Technology"}/>
+                        <div style={{"--w-left": "0%", "--t-align": "left"}}>
+                            <ul>
+                                <li>Bachelor's degree in Computer Science with Software Engineering specialization</li>
+                                <li>Currently enrolled</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={styles.qual_comp_wrapper}>
+                        <QualComponent text={"AUG 2019 - MAY 2023"} desc={"Software Engineering, ZST Chelm"}/>
+                        <div style={{"--w-left": "10%", "--d": "rtl"}}>
+                            <ul>
+                                <li>Graduated with technical diploma in Software Engineering</li>
+                                <li>Two times "Lubelska Kuźnia Talentów" scholarship holder</li>
+                                <li>Second place in Poland-wide contest finale for technical innovation</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <span style={{"--m-left": "-270px", "--w":"300px"}}>certificates</span>
+                </div>
+                <div className={styles.cert_wrapper}>
+                    <div>
+                        <CertIcon/>
+                        <span>AWS Certified Cloud Practicioner CLF-C02</span>
+                    </div>
+                    <div>
+                        <CertIcon/>
+                        <span>INF.03 Professional Qualification</span>
+                    </div>
+                    <div>
+                        <CertIcon/>
+                        <span>INF.04 Professional Qualification</span>
                     </div>
                 </div>
             </div>
